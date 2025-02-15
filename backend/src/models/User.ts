b14e028f2d618603f67d71ad3a2e2b69ceb7b8ba
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2025-02-15 11:40:49 -0300

    refactor: update User model

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,56 +1,8 @@
-import { Model, DataTypes, Optional } from "sequelize";
-import { v4 as uuidv4 } from "uuid";
-import sequelize from "../config/database";
-
-interface UserAttributes {
+export interface User {
   id: string;
-  name: string;
+  username: string;
   email: string;
   password: string;
+  role: "USER" | "ADMIN";
+  createdAt: Date;
 }
-
-interface UserCreationAttributes extends Optional<UserAttributes, "id"> {}
-
-class User
-  extends Model<UserAttributes, UserCreationAttributes>
-  implements UserAttributes
-{
-  public id!: string;
-  public name!: string;
-  public email!: string;
-  public password!: string;
-}
-
-User.init(
-  {
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: uuidv4,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  },
-  {
-    sequelize,
-    tableName: "users",
-    timestamps: true,
-    underscored: true,
-  }
-);
-
-export default User;
