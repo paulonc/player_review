871998d847998acbb6f0a98a102a28e8b7a98e79
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2025-02-15 11:44:39 -0300

    refactor: update index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,45 +1,10 @@
 import * as express from "express";
-import * as dotenv from "dotenv";
-import sequelize from "./config/database";
-import { UserRepository } from "./repository/userRepository";
-
-dotenv.config();
+import userRoutes from "./routes/UserRoutes";
 
 const app = express();
 app.use(express.json());
 
-const userRepo = new UserRepository();
+app.use("/users", userRoutes);
 
-app.post("/users", async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
-
-    const user = await userRepo.createUser(name, email, password);
-    res.status(201).json(user);
-  } catch (error: any) {
-    res
-      .status(500)
-      .json({ message: "Erro ao criar o usuário", error: error.message });
-  }
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const users = await userRepo.getAllUsers();
-    res.status(200).json(users);
-  } catch (error: any) {
-    res
-      .status(500)
-      .json({ message: "Erro ao obter os usuários", error: error.message });
-  }
-});
-
-sequelize
-  .sync({ alter: true })
-  .then(() => {
-    console.log("Banco de dados conectado!");
-    app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-  })
-  .catch((error) => {
-    console.error("Erro ao conectar ao banco de dados:", error);
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
