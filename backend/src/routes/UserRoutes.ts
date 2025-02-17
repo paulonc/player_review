0726adc0d8cb758bc215aec5b6649bfe92a0bb95
alfredo.andrade <alfredo.andrade@wildlifestudios.com>
2025-02-17 16:48:16 -0300

    make the route rest

--- backend/src/routes/UserRoutes.ts
+++ backend/src/routes/UserRoutes.ts
@@ -3,9 +3,9 @@ import UserController from "../controllers/UserController";
 
 const router = Router();
 
-router.post("/register", UserController.register);
-router.get("/:id", UserController.getUser);
 router.get("/", UserController.getAllUsers);
+router.post("/", UserController.register);
+router.get("/:id", UserController.getUser);
 router.put("/:id", UserController.updateUser);
 router.patch("/:id/password", UserController.changePassword);
 router.delete("/:id", UserController.deleteUser);
