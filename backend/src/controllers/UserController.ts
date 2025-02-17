f0224f7dbf1f3e6b3640c340523130940e60aa1d
alfredo.andrade <alfredo.andrade@wildlifestudios.com>
2025-02-17 18:03:32 -0300

    add missing change on user controller

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -65,12 +65,32 @@ class UserController {
     try {
       const user = await UserService.getUserById(id);
 
-      if (!user) return res.status(404).json({ error: "User not found" });
+      if (!user) return res.status(404).json({ error: `User with id ${id} not found` });
 
       await UserService.deleteUser(id);
       return res.status(204).send();
     } catch (error) {
       return res.status(400).json({ error: "Internal server error" });
+    }
+  }
+
+  async updatePassword(req: Request, res: Response): Promise<Response> {
+    const { id } = req.params;
+    const { newPassword } = req.body;
+
+    try {
+      const user = await UserService.getUserById(id);
+
+      if (!user) return res.status(404).json({ error: `User with id ${id} not found` });
+
+      const isPasswordUpdated = await UserService.updatePassword(id, newPassword);
+      if (!isPasswordUpdated) {
+        return res.status(400).json({ error: "Invalid password" });
+      }
+
+      return res.status(200).json({ message: "Password changed successfully" });
+    } catch (error) {
+      return res.status(500).json({ error: "Internal server error" });
     }
   }
 }
