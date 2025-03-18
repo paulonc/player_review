c3c24b94c44117ffac75ed3ea797ac1ccdc9c014
alfredo.andrade <alfredo.andrade@wildlifestudios.com>
2025-03-17 21:19:33 -0300

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRoutes from './routes/UserRoutes';
 import gameRoutes from './routes/GameRoutes';
 import companyRoutes from './routes/CompanyRoutes';
@@ -11,6 +12,24 @@ import { apiLimiter } from './middlewares/rateLimiter';
 
 const app = express();
 app.use(express.json());
+
+const corsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173', 'http://127.0.0.1:3000', 'http://127.0.0.1:5173'];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      logger.warn(`CORS blocked request from origin: ${origin}`);
+      callback(new Error('Not allowed by CORS'), false);
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  maxAge: 86400
+};
+
+app.use(cors(corsOptions));
 
 app.use('/login', authRoutes);
 app.use('/users', userRoutes);
